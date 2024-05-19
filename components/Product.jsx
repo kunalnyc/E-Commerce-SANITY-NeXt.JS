4f90a88c6fb2ea0d306a6def0dd24500402e803d
Kunal <kunalkmr9717@outlook.com>
2024-05-19 21:59:50 +0530

    slug fixing

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
-import Link from 'next/link'
-import { urlFor } from '../lib/client'
-import { urlForImage } from '../sanity/lib/image'
-const Product = ({ product: {
-    image, name, slug, price
-} }) => {
+// components/Product.jsx
+import React from 'react';
+import Link from 'next/link';
+import { urlForImage } from '../sanity/lib/image';
+
+const Product = ({ product: { image, name, slug, price } }) => {
+    console.log(slug.current)
     return (
+
         <div>
             <Link href={`/product/${slug.current}`}>
-                <div className='product-card'>
-                    <img src={urlForImage(image && image[0])} width={250} height={250} className='product-image' />
-                    <p className='product-name'>{name}</p>
-                    <p className='product-name'>${price}</p>
+
+                <div className="product-card">
+                    <img
+                        src={urlForImage(image && image[0])}
+                        width={250}
+                        height={250}
+                        className="product-image"
+                        alt={name}
+                    />
+                    <p className="product-name">{name}</p>
+                    <p className="product-price">${price}</p>
                 </div>
-
-                /</Link>
+            </Link>
         </div>
-    )
-}
+    );
+};
 
-export default Product
+export default Product;
---
+++ product/[slug].js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const ProductDetails = () => {
+  return (
+    <div>
+      <h1>Product Details</h1>
+    </div>
+  );
+};
+
+export default ProductDetails;
