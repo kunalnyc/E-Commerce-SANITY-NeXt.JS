f99f2eeb92b03ee62c539c0d3795913dae5369cd
Kunal <kunalkmr9717@outlook.com>
2024-05-21 13:50:58 +0530

    BEST-SELLERS

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -2,8 +2,12 @@ import React, { Suspense } from 'react';
 import { urlForImage } from '../../../../sanity/lib/image';
 import { fetchProduct } from '../../../../lib/fetchProduct';
 import { Layout } from '@/app/components';
+import { Product } from '@/app/components';
+// importing some react icons to showcase reviews
+import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
+import { fetchRelatedProducts } from '../../../../lib/fetchProduct';
 // Create a component to handle the product details
-function ProductDetails({ product }) {
+function ProductDetails({ product, relatedProducts }) {
   if (!product) {
     return <div>Product not found</div>;
   }
@@ -11,18 +15,60 @@ function ProductDetails({ product }) {
   const { image, name, price, details } = product;
 
   return (
-    <div className='product-detail-container'>
-      <div>
-        <div className='image-container'>
-          {image && image.length > 0 && (
-            <img src={urlForImage(image[0])} alt={name} />
-          )}
+    <div>
+      <div className='product-detail-container'>
+        <div>
+          <div className='image-container'>
+            {image && image.length > 0 && (
+              <img src={urlForImage(image[0])} alt={name} />
+            )}
+          </div>
+          {/* <div className='small-images-container'>
+          {image.map((item, i) => (
+            <img src={urlForImage(item)}
+              className='' onMouseEnter='' />
+          ))}
+
+        </div> */}
+        </div>
+        <div className='product-detail-desc'>
+          <h1>{name}</h1>
+          <div className='reviews'>
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
+            </div>
+            <p>(20)</p>
+          </div>
+          <h4>Details:</h4>
+          <p>{details}</p>
+          <p className='price'>${price}</p>
+          <div className='quantity'>
+            <h3>Quantity:</h3>
+            <p className='quantity-desc'>
+              <span className='minus' onClick=''><AiOutlineMinus /></span>
+              <span className='num' onClick=''>0</span>
+              <span className='plus' onClick=''><AiOutlinePlus /></span>
+            </p>
+          </div>
+          <div className='buttons'>
+            <button type='button' className='add-to-cart' onClick=''>Add to cart</button>
+            <button type='button' className='buy-now' onClick=''>Buy Now</button>
+          </div>
         </div>
       </div>
-      <div className='product-details-desc'>
-        <h1>{name}</h1>
-        <p>{details}</p>
-        <p className='price'>${price}</p>
+      <div className='maylike-products-wrapper'>
+        <h2 > <AiOutlineStar /> BEST-SELLERS / New Arrivals</h2>
+        <div className='marquee' >
+          <div className='maylike-products-container'>
+            {relatedProducts.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -31,7 +77,7 @@ function ProductDetails({ product }) {
 export default async function Page({ params }) {
   const { id } = params;
   const product = await fetchProduct(id);
-
+  const relatedProducts = await fetchRelatedProducts(); // Fetch related products separately
   if (!product) {
     return (
       <Layout>
@@ -43,7 +89,7 @@ export default async function Page({ params }) {
   return (
 
     <Suspense fallback={<div>Loading product details...</div>}>
-      <ProductDetails product={product} />
+      <ProductDetails product={product} relatedProducts={relatedProducts} />
     </Suspense>
 
   );
